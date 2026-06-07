599b67b4a99a9e7159a0f01e4c86300873195f2d
suvarna <suvarna6126@gmail.com>
2026-06-07 23:00:03 +0530

    Day 7 completed

---
+++ Day-7-Arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int longestConsecutive(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // store all elements in set
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for(int num : set) {
+
+            // start of sequence
+            if(!set.contains(num - 1)) {
+
+                int currentNum = num;
+                int count = 1;
+
+                // count consecutive numbers
+                while(set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+}
