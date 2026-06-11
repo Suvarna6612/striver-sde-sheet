56fc0a502b7cc60f2368b411802262ef07020dea
suvarna <suvarna6126@gmail.com>
2026-06-11 21:23:54 +0530

    Day 11 completed - Linked List Part II

---
+++ Day-11-LinkedList/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,39 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+public class IntersectionOfTwoLinkedLists {    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+
+        return a;
+    }
+}
