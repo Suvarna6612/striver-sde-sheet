f18ad2c10b4993bfab3e62f4b35bd58f63ebe927
suvarna <suvarna6126@gmail.com>
2026-07-06 21:58:37 +0530

    Day 36: Added Binary Tree Part II solutions

---
+++ Day-36-Binary-Trees/LevelOrderTraversal.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null)
+            return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null)
+                    q.offer(node.left);
+
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+}
