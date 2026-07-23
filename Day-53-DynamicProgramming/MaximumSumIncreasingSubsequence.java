f2b8d963b81423c656206b87e39922623ca0450a
suvarna <suvarna6126@gmail.com>
2026-07-23 20:48:39 +0530

    Add Day 53 dynamic programming solutions

---
+++ Day-53-DynamicProgramming/MaximumSumIncreasingSubsequence.java
@@ -0,0 +1,27 @@
+
+class Solution {
+
+    public int maxSumIncreasingSubsequence(int[] arr, int n) {
+
+        int[] dp = new int[n];
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            dp[i] = arr[i];
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] < arr[i]) {
+
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+
+            maxSum = Math.max(maxSum, dp[i]);
+        }
+
+        return maxSum;
+    }
+}
